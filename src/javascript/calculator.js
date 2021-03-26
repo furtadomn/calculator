fee543e3b612d9a456679b5eb276a2948b322bb5
Marcella Furtado <mnfurtado@yahoo.com.br>
2021-03-26 15:33:07 -0300

    commit inicial

---
+++ src/javascript/calculator.js
@@ -0,0 +1,8 @@
+function clean() {
+    let C = window.document.querySelector('input.display').value=""
+}
+
+function button(number) {
+    let B = window.document.querySelector('input.display')
+    B.value = B.value + number
+}
